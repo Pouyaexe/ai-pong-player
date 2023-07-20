1a331837fa304372322a3c0a524ed3b320156c32
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2023-07-20 07:27:38 +0000

    Jul 20, 2023, 10:57 AM

--- tutorial.py
+++ tutorial.py
@@ -10,4 +10,17 @@ from pong import Game
 width, height = 700, 500 # Screen size
 window = pygame.display.set_mode((width, height))
 
-game = Game(window=window, width=width, height=height)
+game = Game(window=window, window_width=width, window_height=height)
+
+run = True
+clock = pygame.time.Clock()
+while run:
+    clock.tick(60) # Max number of frames per second 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # If user clicks close (x)
+            run = False
+    
+    game.loop()
+    game.draw()
+    pygame.display.update() # Draw the screen
+

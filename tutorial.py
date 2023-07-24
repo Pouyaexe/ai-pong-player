fb2f41a2d47cfb7593c8f780d58ea29e5790a982
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2023-07-24 07:04:36 +0000

    Jul 24, 2023, 10:34 AM

--- tutorial.py
+++ tutorial.py
@@ -1,120 +1,142 @@
-"""Example of how to make an AI that plays pong game.
-"""
-import pickle
-import os
-import neat
 import pygame
 from pong import Game
+import neat
+import os
+import pickle
 
 
 class PongGame:
     def __init__(self, window, width, height):
-        self.game = Game(window=window, window_width=width, window_height=height)
-        self.left_paddle = self.game.left_paddle  # location of left paddle
-        self.right_paddle = self.game.right_paddle  # location of right paddle
-        self.ball = self.game.ball  # location of ball
+        self.game = Game(window, width, height)
+        self.left_paddle = self.game.left_paddle
+        self.right_paddle = self.game.right_paddle
+        self.ball = self.game.ball
 
-    def test_ai(self):
-        """Test the AI by playing the game."""
+    def test_ai(self, genome, config):
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+
         run = True
         clock = pygame.time.Clock()
         while run:
-            clock.tick(60)  # Max number of frames per second
+            clock.tick(60)
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:  # If user clicks close (x)
+                if event.type == pygame.QUIT:
                     run = False
                     break
 
             keys = pygame.key.get_pressed()
             if keys[pygame.K_w]:
-                game.move_paddle(left=True, up=True)
-
+                self.game.move_paddle(left=True, up=True)
             if keys[pygame.K_s]:
-                game.move_paddle(left=True, up=False)
+                self.game.move_paddle(left=True, up=False)
 
-            game_info = game.loop()
-            print(game_info.left_score, game_info.right_score)
-            game.draw(False, True)
-            pygame.display.update()  # Draw the screen
+            output = net.activate(
+                (self.right_paddle.y, self.ball.y, abs(self.right_paddle.x - self.ball.x)))
+            decision = output.index(max(output))
 
-        pygame.quit()  # Close the window. Outside the while loop
+            if decision == 0:
+                pass
+            elif decision == 1:
+                self.game.move_paddle(left=False, up=True)
+            else:
+                self.game.move_paddle(left=False, up=False)
+
+            game_info = self.game.loop()
+            self.game.draw(True, False)
+            pygame.display.update()
+
+        pygame.quit()
+
     def train_ai(self, genome1, genome2, config):
         net1 = neat.nn.FeedForwardNetwork.create(genome1, config)
         net2 = neat.nn.FeedForwardNetwork.create(genome2, config)
-        
-        
+
         run = True
         while run:
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:  # If user clicks close (x)
+                if event.type == pygame.QUIT:
                     quit()
-            output1 = net1.activate((self.left_paddle.y, self.ball.y, abs(self.left_paddle.x - self.ball.x)))        
-            output2 = net2.activate((self.right_paddle.y, self.ball.y, abs(self.right_paddle.x - self.ball.x)))
-            
-            game_info = self.game.loop()
-            self.game.draw()
+
+            output1 = net1.activate(
+                (self.left_paddle.y, self.ball.y, abs(self.left_paddle.x - self.ball.x)))
+            decision1 = output1.index(max(output1))
+
+            if decision1 == 0:
+                pass
+            elif decision1 == 1:
+                self.game.move_paddle(left=True, up=True)
+            else:
+                self.game.move_paddle(left=True, up=False)
+
+            output2 = net2.activate(
+                (self.right_paddle.y, self.ball.y, abs(self.right_paddle.x - self.ball.x)))
+            decision2 = output2.index(max(output2))
+
+            if decision2 == 0:
+                pass
+            elif decision2 == 1:
+                self.game.move_paddle(left=False, up=True)
+            else:
+                self.game.move_paddle(left=False, up=False)
+
+            game_info = game.loop()
+
+            game.draw(draw_score=False, draw_hits=True)
             pygame.display.update()
-            
-            if game_info.left_score >= 1 or game_info.right_score >= 1:
+
+            if game_info.left_score >= 1 or game_info.right_score >= 1 or game_info.left_hits > 50:
                 self.calculate_fitness(genome1, genome2, game_info)
                 break
-    
+
     def calculate_fitness(self, genome1, genome2, game_info):
-        pass
-                    
+        genome1.fitness += game_info.left_hits
+        genome2.fitness += game_info.right_hits
+
 
 def eval_genomes(genomes, config):
-    """
-    Run each genome against eachother one time to determine the fitness.
-    """
     width, height = 700, 500
-    win = pygame.display.set_mode((width, height))
-    pygame.display.set_caption("Pong")
+    window = pygame.display.set_mode((width, height))
 
     for i, (genome_id1, genome1) in enumerate(genomes):
-        print(round(i/len(genomes) * 100), end=" ")
+        if i == len(genomes) - 1:
+            break
         genome1.fitness = 0
-        for genome_id2, genome2 in genomes[min(i+1, len(genomes) - 1):]:
+        for genome_id2, genome2 in genomes[i+1:]:
             genome2.fitness = 0 if genome2.fitness == None else genome2.fitness
-            pong = PongGame(win, width, height)
+            game = PongGame(window, width, height)
+            game.train_ai(genome1, genome2, config)
 
-            force_quit = pong.train_ai(genome1, genome2, config, draw=True)
-            if force_quit:
-                quit()
-    
 
 def run_neat(config):
-    # p = neat.Checkpointer.restore_checkpoint("neat-checkpoint-0") # Load the last checkpoint
-    p = neat.Population(
-        config
-    )  # Create a new population, comment this line if you want to load the last checkpoint
+    p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-7')
+    #p = neat.Population(config)
     p.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
-    p.add_reporter(neat.Checkpointer(1))  # Save the best genome every 1 generation
+    p.add_reporter(neat.Checkpointer(1))
 
-    winner = p.run(eval_genomes, 50)
-    
-    
+    winner = p.run(eval_genomes, 1)
+    with open("best.pickle", "wb") as f:
+        pickle.dump(winner, f)
+
+
+def test_ai(config):
+    width, height = 700, 500
+    window = pygame.display.set_mode((width, height))
+
+    with open("best.pickle", "rb") as f:
+        winner = pickle.load(f)
+
+    game = PongGame(window, width, height)
+    game.test_ai(winner, config)
+
 
 if __name__ == "__main__":
-    local_dir = os.path.dirname(__file__)  # Get the current directory
-    config_path = os.path.join(
-        local_dir, "config.txt"
-    )  # Get the path to the config file
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config.txt")
 
-    config = neat.config.Config(
-        neat.DefaultGenome,
-        neat.DefaultReproduction,
-        neat.DefaultSpeciesSet,
-        neat.DefaultStagnation,
-        config_path,
-    )  # Load the config file
-
-    run_neat(config)
-
-# # Global variables
-# width, height = 700, 500 # Screen size
-# window = pygame.display.set_mode((width, height))
-
-# game = Game(window=window, window_width=width, window_height=height)
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+    # run_neat(config)
+    test_ai(config)
